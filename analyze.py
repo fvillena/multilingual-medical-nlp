81f11af4c656a873f0c9550375817e978b988afd
Fabián Villena <villenafabian@gmail.com>
2019-09-18 16:21:35 +0200

    text processing pipeline

--- analyze.py
+++ analyze.py
@@ -1,5 +1,8 @@
 from src.data.gms_data import GmsParser
+from src.data.text_data import TextProcessor
 
 gms_parser = GmsParser(r"data/raw/gms/")
-
 gms_parser.parse(r"data/interim/")
+
+processor = TextProcessor(r'data/interim/')
+processor.process()
