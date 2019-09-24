a9c71eaeea04adc6359ac225c2726aeb9fef7eb8
Fabián Villena <villenafabian@gmail.com>
2019-09-24 15:49:57 +0200

    adding spanish and writing the data in the process method

--- src/data/text_data.py
+++ src/data/text_data.py
@@ -1,19 +1,26 @@
 import os
 import json
+import hashlib
 from spacy.lang.fr import French
 from spacy.lang.de import German
 from spacy.lang.en import English
-def tokenize(document,language):
+from spacy.lang.es import Spanish
+def tokenize(document,language,punctutation):
     if language == 'fr':
         nlp = French()
     if language == 'de':
         nlp = German()
     if language == 'en':
         nlp = French()
+    if language == 'es':
+        nlp = Spanish()
     sentencizer = nlp.create_pipe("sentencizer")
     nlp.add_pipe(sentencizer)
     doc = nlp(document)
-    sentences = [[str(word) for word in sent if str(word) != '\n'] for sent in doc.sents]
+    if punctutation:
+        sentences = [[str(word) for word in sent if str(word) != '\n'] for sent in doc.sents]
+    else:
+        sentences = [[str(word) for word in sent if ((str(word) != '\n') and (str(word).isalpha()))] for sent in doc.sents]
     return sentences
 
 class TextProcessor:
@@ -27,9 +34,14 @@ class TextProcessor:
                     self.documents[language].append(document['body'])
                 else:
                     self.documents[language] = [document['body']]
-    def process(self, normalize=False, punctutation=True):
-        self.corpora = {}
+    def process(self, output_folder, normalize=False, punctutation=True):
         for language,corpus in self.documents.items():
-            self.corpora[language] = []
             for document in corpus:
-                self.corpora[language].append(tokenize(document,language))
+                tokenized_document = tokenize(document,language,punctutation)
+                document_to_write = []
+                for sentence in tokenized_document:
+                    sentence = ' '.join(sentence)
+                    document_to_write.append(sentence)
+                document_to_write = '\n'.join(document_to_write)
+                with open(output_folder + language + '/' + hashlib.md5(document_to_write.encode('utf-8')).hexdigest() +'.txt', 'w') as f:
+                    f.write(document_to_write)
