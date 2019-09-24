d326e1381d76b000cd9d5eea0e2af0a38dab1533
Fabián Villena <villenafabian@gmail.com>
2019-09-24 17:22:15 +0200

    adding scielo and text processer and analyzer

--- analyze.py
+++ analyze.py
@@ -1,8 +1,15 @@
 from src.data.gms_data import GmsParser
+from src.data.scielo_data import ScieloParser
 from src.data.text_data import TextProcessor
+from src.data.text_data import TextAnalyzer
 
 gms_parser = GmsParser(r"data/raw/gms/")
 gms_parser.parse(r"data/interim/")
 
+scielo_parser = ScieloParser(r"data/raw/scielo/")
+scielo_parser.parse(r"data/interim/")
+
 processor = TextProcessor(r'data/interim/')
-processor.process()
+processor.process(output_folder = r'data/processed/', punctutation=False)
+
+analyzer = TextAnalyzer(r'data/processed/')
